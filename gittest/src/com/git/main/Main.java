1425754af9b86113692962d021ceb8344d2dcdee
yoss <yoss@windows10.microdone.cn>
2021-04-27 18:17:22 +0900

    #07 controller클래스추가, test()추가 -ys-

--- gittest/src/com/git/main/Main.java
+++ gittest/src/com/git/main/Main.java
@@ -1,13 +1,15 @@
 package com.git.main;
 
-import java.util.Scanner;
+import java.util.Date;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		var a=10;
-		Scanner sc= new Scanner(System.in);
+		System.out.println("안녕 git 반가워~ 오랜만이야!!");
+		
+		Date today= new Date();
+		System.out.println(today);
 
 	}
 
