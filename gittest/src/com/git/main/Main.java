bc67b337816b09916f908ef31f9bcbb2c5587a05
yoss <yoss@windows10.microdone.cn>
2021-04-27 18:03:26 +0900

    #04 오늘날짜 출력문추가 -용승-

--- gittest/src/com/git/main/Main.java
+++ gittest/src/com/git/main/Main.java
@@ -1,10 +1,15 @@
 package com.git.main;
 
+import java.util.Date;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("안녕 git 반가워~ 오랜만이야!!");
+		
+		Date today= new Date();
+		System.out.println(today);
 
 	}
 
